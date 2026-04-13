3827dc8b1fb34420ac9d1ad3a0f5bca4f2085fda
mikre44 <miastek44@gmail.com>
2026-04-13 04:23:42 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,170 @@
+
+import 'dotenv/config';
+
+import express from "express";
+import categories from "./models/categories.js";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import settings from "./models/settings.js";
+import session from "./models/session.js";
+import auth from "./controllers/auth.js";
+import user from "./models/user.js";
+
+const port = process.env.PORT || 8000;
+const ONE_DAY = 24 * 60 * 60 * 1000;
+const ONE_MONTH = 30 * ONE_DAY;
+const SECRET = process.env.SECRET;
+
+if (SECRET == null) {
+  console.error(
+    `SECRET environment variable missing.
+     Please create an env file or provide SECRET via environment variables.`,
+  );
+  process.exit(1);
+}
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded());
+app.use(morgan("dev"));
+app.use(cookieParser(SECRET));
+
+app.use(settings.settingsHandler);
+app.use(session.sessionHandler);
+
+const settingsRouter = express.Router();
+settingsRouter.use("/toggle-theme", settings.themeToggle);
+app.use("/settings", settingsRouter);
+
+const authRouter = express.Router();
+authRouter.get("/signup", auth.signup_get);
+authRouter.post("/signup", auth.signup_post);
+authRouter.get("/login", auth.login_get);
+authRouter.post("/login", auth.login_post);
+authRouter.get("/logout", auth.logout);
+authRouter.get("/account", auth.account_get);
+app.use("/auth", authRouter);
+
+
+
+function requireLogin(req, res, next) {
+  if (req.path.startsWith("/auth")) return next();
+
+  if (res.locals.user == null) {
+    return res.redirect("/auth/login");
+  }
+
+  return next();
+}
+
+app.use(requireLogin);
+
+
+
+
+app.get("/:category_name", (req, res) => {
+  const category_name = req.params.category_name;
+  if (!categories.getCategory(category_name)) {
+    res.sendStatus(404);
+  } else {
+    const userId = session.getUserId(req)
+    const cards = categories.getCards(category_name, req).cards;
+    const categoriesList = categories.getCategorySummaries(userId).categories;
+    const currentUser = user.getUser(userId).username;
+
+    if (cards != null) {
+      res.render("cards", {
+        title: category_name,
+        cards,
+        page: req.originalUrl,
+        categories: categoriesList,
+        username: currentUser,
+      });
+    } else {
+      res.sendStatus(404);
+    }
+  }
+});
+
+app.post("/:category_name/new", (req, res) => {
+  
+
+  const currentCategory = req.params.category_name;
+  if (!categories.getCategory(currentCategory)) {
+    res.sendStatus(404);
+  } else {
+
+    let categoryNames = req.body.categories || [];
+
+    if (!Array.isArray(categoryNames)) {
+      categoryNames = [categoryNames];
+    }
+    const published = req.body.published ? 1 : 0;
+    categories.addCard(categoryNames, {
+      name: req.body.name,
+      link: req.body.link,  
+    },
+    published,
+    req
+  );
+    res.redirect(`/${currentCategory}`);
+  }
+});
+
+app.post("/:category_name/delete", (req, res) => {
+
+  const currentCategory = req.params.category_name;
+  const delete_id = req.body.delete_id
+
+  if(categories.getCardById(delete_id)) {
+    categories.deleteCard(req, delete_id);
+  }
+  if (categories.getCategory(currentCategory)) {
+    return res.redirect(`/${currentCategory}`);
+  }
+  return res.redirect(`/`);
+
+  
+});
+
+app.post("/newCategory", (req, res) => {
+  const published = req.body.published ? 1 : 0;
+
+  categories.addCategory(req, res, published);
+
+});
+app.post("/deleteCategory", (req, res) => {
+  const delete_id = req.body.delete_id
+
+  categories.deleteCategory(req, delete_id);
+  return res.redirect(`/`);
+
+});
+
+
+app.get("/", (req, res) => {
+
+  const userId = session.getUserId(req);
+  const categoriesList = categories.getCategorySummaries(userId).categories;
+  const currentUser = user.getUser(userId);
+
+  if(req.query.error) var error = true;
+  else error = false;
+
+  res.render("index", {
+    title: "Home",
+    categories: categoriesList,
+    page: req.originalUrl,
+    username: currentUser.username,
+    error,
+  });
+});
+
+
+
+
+app.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}`);
+});
+
---
+++ models/categories.js
@@ -0,0 +1,312 @@
+
+import { DatabaseSync } from "node:sqlite";
+import session, { getUserId } from "./session.js";
+import { isAdmin } from "./user.js";
+
+
+
+
+const db_path = "./db.sqlite";
+const db = new DatabaseSync(db_path);
+
+
+const categories = {
+  "games": {
+    title: "games",
+    link: [
+      { name: "Slope", link: "https://slopegame.io" },
+      { name: "super mario bros", link: "https://supermarioplay.com" },
+      { name: "Geoguessr", link: "https://www.geoguessr.com" },
+      { name: "Little Alchemy 2", link: "https://littlealchemy2.com" },
+
+    ],
+  },
+  "sites": {
+    title: "sites",
+    link: [ 
+      { name: "W3Schools", link: "https://www.w3schools.com" },
+      { name: "FreeCodeCamp", link: "https://www.freecodecamp.org" },
+      { name: "MDN Web Docs", link: "https://developer.mozilla.org" },
+      { name: "Codewars", link: "https://www.codewars.com" },
+    ],
+  },
+};
+
+db.exec('PRAGMA foreign_keys = ON;');
+
+db.exec(`
+  CREATE TABLE IF NOT EXISTS categories (
+    id          INTEGER PRIMARY KEY,
+    name        TEXT NOT NULL UNIQUE,
+    user_id     INTEGER NOT NULL,
+    published   INTEGER DEFAULT 0,
+    created_at      INTEGER
+  ) STRICT;
+`);
+
+
+db.exec(`
+  CREATE TABLE IF NOT EXISTS links (
+    id          INTEGER PRIMARY KEY,
+    name        TEXT NOT NULL,
+    link        TEXT NOT NULL,
+    user_id     INTEGER NOT NULL,
+    published   INTEGER DEFAULT 0,
+    created_at  INTEGER
+  ) STRICT;
+`);
+
+db.exec(`
+  CREATE TABLE IF NOT EXISTS categories_links (
+    category_id INTEGER NOT NULL,
+    link_id     INTEGER NOT NULL,
+    PRIMARY KEY (category_id, link_id),
+    FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE,
+    FOREIGN KEY (link_id)     REFERENCES links(id)     ON DELETE CASCADE
+  ) STRICT;
+`);
+
+
+const db_ops = {
+
+  get_categories: db.prepare(
+    "SELECT DISTINCT id, name, user_id FROM categories WHERE user_id = ? OR published >= ?;"
+  ),
+  get_cards: db.prepare(
+    `SELECT links.id, links.name, link, links.user_id FROM links
+    JOIN categories_links ON links.id = categories_links.link_id
+    JOIN categories ON categories_links.category_id = categories.id
+    WHERE categories.name = ? AND (links.user_id = ? OR links.published >= ?)`
+  ),
+  get_card_by_id: db.prepare(
+  `SELECT * FROM links WHERE id = ?;`
+  ),
+  get_category: db.prepare(
+    `SELECT * FROM categories WHERE name = ?;`
+  )
+};
+
+
+
+
+// process.env.POPULATE_DB = "1";
+// ^^^ na windowsie działa
+
+
+const checkForMinimumData =  
+  db.prepare(
+    `SELECT * FROM 'links';`
+  ).all();
+
+if(checkForMinimumData.length === 0 ||  process.env.POPULATE_DB){
+  console.log("Populating db...");
+
+  Object.values(categories).forEach((category) => {
+
+    const insertedCategory = db
+    .prepare(
+      `INSERT INTO categories (name, user_id, published)
+      VALUES (?, 1, 1);`
+    )
+    .run(category.title);
+    console.log("Inserted to categories:", {
+    category: category.title,
+    });
+
+
+    category.link.forEach((item) => {
+      const insertedLink  = db
+      .prepare(
+            `INSERT INTO links (name, link, user_id, published)
+            VALUES (?, ?, 1, 1);`
+        ) .run(item.name, item.link);
+        
+      const linkId = insertedLink.lastInsertRowid;
+      console.log("Inserted link:", {
+        id: linkId,
+        category: category.title,
+        name: item.name,
+        link: item.link
+      });
+      
+      const categoryId = db_ops.get_category.get(category.title).id;
+
+      db.prepare(
+        `INSERT INTO categories_links (category_id, link_id)
+        VALUES (?, ?);`
+      ).run(categoryId, linkId);
+
+      console.log("Inserted foreign key:", {
+        category_id: categoryId,
+        link_id: linkId
+      });
+    });
+    
+  });
+}
+
+
+
+
+
+
+export function getCategorySummaries(userId) {
+    if (isAdmin(userId)){
+    var mustBePublished = 0;
+  }
+  else{
+    var mustBePublished = 1;
+  } 
+  const categories = db_ops.get_categories.all(userId, mustBePublished);
+  if (categories.length === 0) {
+    return{categories: 0};
+  }
+  return {
+    categories: categories.map(category => ({
+      id: category.id,
+      name: category.name,
+      parent: category.user_id == userId || mustBePublished == 0
+    }))
+  };
+}
+
+export function getCategory(categoryName){
+    const category = db_ops.get_category.get(categoryName);
+  return category;
+}
+
+export function getCards(categoryName, req) {
+
+  let userId = getUserId(req)
+  if (isAdmin(userId)){
+    var mustBePublished = 0;
+  }
+  else{
+    var mustBePublished = 1;
+  }
+
+
+  const cards = db_ops.get_cards.all(categoryName, userId, mustBePublished);
+
+  if (cards.length === 0) {
+    return{cards: 0};
+  }
+  return {
+    cards: cards.map(card => ({
+      id: card.id, 
+      name: card.name,
+      link: card.link,
+      parent: card.user_id == userId || mustBePublished === 0
+    }))
+  };
+}
+
+export function getCardById(cardId){
+  const card = db_ops.get_card_by_id.get(cardId)
+  return card;
+}
+
+export function addCard(categoryNames, card, published, req) {
+  
+
+  categoryNames.forEach(name => {
+    
+    let categoryid = db.prepare(
+    `SELECT id FROM categories WHERE name = ?;`
+    ).get(name).id;
+    
+    if (categoryid != null){
+      var isInserted = 0
+      if(isInserted == 0){
+        const userId = getUserId(req);
+
+        let insertedLink = db
+        .prepare(
+        `INSERT INTO links (name, link, user_id, published)
+        VALUES (?, ?, ?, ?) RETURNING id, name, link;`
+        ).get(card.name, card.link, userId, published);
+        var linkId = insertedLink.id
+        isInserted++;
+      }
+      
+      db.prepare (`INSERT INTO categories_links (category_id, link_id) 
+      VALUES (?, ?);`).run(categoryid, linkId);
+        
+    }
+  });
+  
+}
+export function deleteCard(req, cardId){
+    const userId = getUserId(req);
+    let card;
+    if (isAdmin(userId)){
+      card = db.
+      prepare("SELECT * FROM links WHERE id = ?")
+      .get(cardId);
+    }
+    else{
+
+      card = db.
+      prepare("SELECT * FROM links WHERE id = ? AND user_id = ?;")
+      .get(cardId, userId);
+    }
+
+    if (card){
+      db.prepare(
+      `DELETE FROM links WHERE id = ?;`
+      ).run(cardId)
+    }
+}
+
+export function addCategory(req, res, published) {
+  const userId = getUserId(req);
+
+  if (req.body.name && userId) {
+    const availableName = !db_ops.get_category.get(req.body.name);
+
+    if (availableName) {
+      db.prepare(`
+        INSERT INTO categories (name, user_id, published)
+        VALUES (?, ?, ?);
+      `).run(req.body.name, userId, published);
+      
+      res.redirect("/");
+    }else{
+      return res.redirect("/?error=zajeta_nazwa");
+    }
+  }
+}
+
+
+export function deleteCategory(req, categoryId) {
+  const userId = getUserId(req);
+    let category;
+    if (isAdmin(userId)){
+      category = db
+      .prepare("SELECT * FROM categories WHERE id = ?;")
+      .get(categoryId);
+    }
+    else{
+
+      category = db.
+      prepare("SELECT * FROM categories WHERE id = ? AND user_id = ?;")
+      .get(categoryId, userId);
+    }
+
+    if (category){
+      db.prepare(
+      `DELETE FROM categories WHERE id = ?;`
+      ).run(categoryId)
+    }
+}
+
+export default {
+  getCategorySummaries,
+  getCards,
+  getCardById,
+  addCard,
+  deleteCard,
+  getCategory,
+  addCategory,
+  deleteCategory
+};
